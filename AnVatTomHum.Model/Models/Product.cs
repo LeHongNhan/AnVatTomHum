c0368643e085561b96d6c7b76b474bd8b2a15d17
Nguyn1710 <91448014+LeHongNhan@users.noreply.github.com>
2025-02-11 23:42:23 +0700

    Add Models and Abstracts to Model Project

---
+++ AnVatTomHum.Model/Models/Product.cs
@@ -0,0 +1,40 @@
+﻿using AnVatTomHum.Model.Abstracts;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace AnVatTomHum.Model.Models
+{
+    [Table("Products")]
+    public class Product : Auditable
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string Alias { get; set; }
+
+        public int CategoryID { get; set; }
+        public string Image { get; set; }
+        public XElement MoreImage { get; set; }
+        
+        public Decimal Price { get; set; }
+
+        public Decimal? PromotionPrice { get; set; } 
+        public string Description { get; set; }
+        public string Content { get; set; }
+        public bool? HomeFlag { get; set; }
+        public bool? HotFlag { get; set; }
+        public int ViewCount {  get; set; }
+        [ForeignKey("CategoryID")]
+        public virtual ProductCategory ProductCategory { get; set; }
+
+    }
+}
